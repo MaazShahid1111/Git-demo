a0f70b270be3bc86540b32490fc0770dda363353
MaazShahid1111 <sirmaazshahid1111@gmail.com>
2024-11-12 09:20:42 +0500

    test 2

---
+++ maaz.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    printf("Enter a number :");
+    int n;
+    scanf("%d",&n);
+    if (n>0)
+    {
+        printf("The number is positive.");
+    }
+    else 
+    printf("The number is negative.")
+    return 0;
+}
